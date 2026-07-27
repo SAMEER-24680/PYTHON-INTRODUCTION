e7f7ab860c85e763bef50a1dc17229034feab660
SAMEER-24680 <sumairasohail523@gmail.com>
2026-07-27 20:44:51 +0500

    Add files via upload

---
+++ variable_activity2.py
@@ -0,0 +1,10 @@
+A = 1
+B = 2
+C = 3
+print (A)
+print (B)
+print (C)
+codingal = "CODINGAL"
+print (codingal)
+name = input("What's your name")
+print ("\n HELLO", name, "\nWellcome to codingal", end = ".")
